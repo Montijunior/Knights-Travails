bd32ac47438a06866d7c642c8f8c6ca660f8ad36
Monti junior <montiabouou@gmail.com>
2024-06-25 12:51:36 -0800

    Helper functions added

--- index.js
+++ index.js
@@ -18,3 +18,67 @@ function getIndex(board, target) {
     }
   }
 }
+
+// breath first search
+function buildSearchArray(board, startIndex) {
+  let array = [];
+  for (let i = 0; i < board.length; i++) {
+    array[i] = {
+      distance: null,
+      parent: null,
+    };
+  }
+  array[startIndex].distance = 0;
+  return array;
+}
+
+// return list of all neighbors of the next moves
+function buildAdjacentList(board) {
+  let adjacentList = [];
+  for (let i = 0; i < board.length; i++) {
+    let neighbors = [];
+    for (let j = 0; j < 8; j++) {
+      let neighbor = findNextMove(j, board[i][0], board[i][1]);
+      if (containsSpot(neighbor)) {
+        neighbors.push(getIndex(board, neighbor));
+      }
+    }
+    adjacentList[i] = neighbors;
+  }
+  return adjacentList;
+}
+
+// All possible moves
+function findNextMove(index, x, y) {
+  const directions = [
+    [2, 1],
+    [1, 2],
+    [-1, 2],
+    [-2, 1],
+    [-2, -1],
+    [-1, -2],
+    [1, -2],
+    [2, -1],
+  ];
+  return [x + directions[index][0], y + directions[index][1]];
+}
+
+// move exist?
+function containsSpot(target) {
+  return target[0] > 0 && target[0] < 8 && target[1] > 0 && target[1] < 8;
+}
+
+// create Path
+function createPath(board, infoArray, item, index, newArray) {
+  if (item.parent !== null) {
+    newArray.push(board[index]);
+    createPath(board, infoArray, infoArray[item.parent], item.parent, newArray);
+  }
+}
+
+// KnightMoves()
+function KnightMoves(start, end) {
+  const board = buildBoard();
+  const startIndex = getIndex(board, start);
+  const endIndex = getIndex(board, end);
+}
